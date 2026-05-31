511251da7aa1f5a1196acac8140372a149573958
stephen-data-engineer-public <resgad.inc@gmail.com>
2026-05-31 14:11:42 +0100

    ALTER_VIEW | Stephen_PC.DWH_PRESENTATION.dbo.VW_TEST | Stephen_PC\resga | 2026-05-31 14:11:36

---
+++ Stephen_PC/DWH_PRESENTATION/dbo/views/VW_TEST/VW_TEST_current.sql
@@ -0,0 +1,17 @@
+﻿ALTER VIEW VW_TEST
+AS
+
+
+SELECT  top 10 [ID]
+      ,[EventType]
+      ,[ObjectName]
+      ,[ObjectType]
+      ,[SQLCommand]
+      ,[ChangedBy]
+      ,[ChangeDate]
+      ,[Committed]
+      ,[DatabaseName]
+      ,[ServerName]
+      ,[SchemaName]
+  FROM [DWH_PRESENTATION].[dbo].[DDL_AuditLog]
+
