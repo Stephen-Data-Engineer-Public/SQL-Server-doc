133afeff76e9476b0245d19e0033c648598b1e43
stephen-data-engineer-public <resgad.inc@gmail.com>
2026-05-31 16:13:10 +0100

    Manual: customers table backlog

---
+++ Stephen_PC/DWH_PRESENTATION/dbo/tables/customers/customers_current.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[customers](
+	[customer_id] [smallint] NOT NULL,
+	[name] [nvarchar](50) NOT NULL,
+	[email] [nvarchar](50) NOT NULL,
+	[blacklisted] [bit] NOT NULL,
+	[customer_address] [nvarchar](50) NOT NULL
+) ON [PRIMARY]
