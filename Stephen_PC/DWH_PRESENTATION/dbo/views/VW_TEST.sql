1d5a4a131a23a90adfb2f4f2c9d49b316b007e8f
stephen-data-engineer-public <resgad.inc@gmail.com>
2026-05-31 13:53:42 +0100

    Auto: [Stephen_PC\DWH_PRESENTATION] VIEW [dbo].VW_TEST modified by Stephen_PC\resga on 05/31/2026 13:15:47

---
+++ Stephen_PC/DWH_PRESENTATION/dbo/views/VW_TEST.sql
@@ -0,0 +1,17 @@
+﻿CREATE VIEW VW_TEST
+AS
+
+
+SELECT TOP (1000) [ID]
+      ,[EventType]
+      ,[ObjectName]
+      ,[ObjectType]
+      ,[SQLCommand]
+      ,[ChangedBy]
+      ,[ChangeDate]
+      ,[Committed]
+      ,[DatabaseName]
+      ,[ServerName]
+      ,[SchemaName]
+  FROM [DWH_PRESENTATION].[dbo].[DDL_AuditLog]
+
