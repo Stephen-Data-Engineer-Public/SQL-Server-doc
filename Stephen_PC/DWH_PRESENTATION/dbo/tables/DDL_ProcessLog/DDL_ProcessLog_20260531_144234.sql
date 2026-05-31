84f86f389e9737d140a3fccd62309736944a5097
stephen-data-engineer-public <resgad.inc@gmail.com>
2026-05-31 14:42:54 +0100

    CREATE_TABLE | Stephen_PC.DWH_PRESENTATION.dbo.DDL_ProcessLog | Stephen_PC\resga | 2026-05-31 14:42:34

---
+++ Stephen_PC/DWH_PRESENTATION/dbo/tables/DDL_ProcessLog/DDL_ProcessLog_20260531_144234.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE dbo.DDL_ProcessLog (
+    LogID           INT IDENTITY PRIMARY KEY,
+    AuditLogID      INT NOT NULL,
+    ServerName      NVARCHAR(200),
+    DatabaseName    NVARCHAR(200),
+    SchemaName      NVARCHAR(100),
+    ObjectName      NVARCHAR(200),
+    EventType       NVARCHAR(100),
+    VersionedFile   NVARCHAR(500),
+    GitCommitMsg    NVARCHAR(1000),
+    ProcessedAt     DATETIME DEFAULT GETDATE(),
+    Status          NVARCHAR(50),
+    ErrorMessage    NVARCHAR(MAX) NULL
+)
